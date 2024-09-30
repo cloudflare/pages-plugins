6143e28528d8c82399925b272d4aa81ed2ce6ce5
Greg Brimble <gbrimble@cloudflare.com>
2024-09-30 00:53:29 -0400

    Supress semgrep

--- example/functions/hcaptcha.ts
+++ example/functions/hcaptcha.ts
@@ -2,6 +2,7 @@ import hCaptchaPlugin from "@cloudflare/pages-plugin-hcaptcha";
 
 export const onRequest: PagesFunction[] = [
   hCaptchaPlugin({
+    // nosemgrep
     secret: "0x0000000000000000000000000000000000000000",
     sitekey: "10000000-ffff-ffff-ffff-000000000001",
     response: "10000000-aaaa-bbbb-cccc-000000000001",
